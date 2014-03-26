d23ba35db9c1b3f78a211f6bf86d701746d99f52
Md Zahed Hossain <mdzahedhossain@yahoo.com>
2014-03-25 21:02:34 -0400

    create index.php file

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+
+<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
+<meta name="author" content="" />
+
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+
+<title>1stWebDesigner PHP Template</title>
+
+</head>
+
+	<body>
+
+		<div id="wrapper">
+
+<div id="header">
+
+</div> <!-- end #header -->
+
+<div id="nav">
+
+</div> <!-- end #nav -->
+
+<div id="content">
+
+</div> <!-- end #content -->
+
+<div id="sidebar">
+
+</div> <!-- end #sidebar -->
+
+<div id="footer">
+
+</div> <!-- end #footer -->
+
+		</div> <!-- End #wrapper -->
+
+	</body>
+
+</html>
